5512c5ece5d15a8a29d694c6bf91239888ff9b99
Paul Nichols <pnichols104@gmail.com>
2013-04-06 15:43:10 -0400

    Fixed link on abbreviated blog posts.

--- blog/templatetags/catalog_filters.py
+++ blog/templatetags/catalog_filters.py
@@ -1,11 +1,13 @@
 from django import template
-
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.filter(name='shorten')
-def shorten(content):
-	#will show up to the first 500 characters of the post content
-	if len(content) > 500:
-		return content[:500] + '....' #+ <a href="/entry/{{post.id}}">(cont.)</a>
-	else:
-		return content
+def shorten(content, post_id):
+    #will show up to the first 500 characters of the post content
+    if len(content) > 500:
+        output = "{0}... <a href='/entry/{1}'>(cont.)</a>".format(content[:500], post_id)
+    else:
+        output = "{0}".format(content)
+
+    return mark_safe(output)
